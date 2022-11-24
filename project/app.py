d41a833474f6c9d2dfbaee5e11b3d759e7dca677
P1atyphy11a <lhy010108@outlook.com>
2022-11-23 22:20:43 -0500

    SQLAlchemy

--- project/app.py
+++ project/app.py
@@ -1,56 +1,54 @@
-from flask import Flask, g, render_template, request, session, flash, redirect, abort, url_for, jsonify
-
 import sqlite3
+from pathlib import Path
 
-#configuration
+from flask import Flask, g, render_template, request, session, \
+                  flash, redirect, url_for, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = Path(__file__).resolve().parent
+
+# configuration
 DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
-SECRET_KEY = "12345"
+SECRET_KEY = "change_me"
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#create and initialize a new Flask app
+
+# create and initialize a new Flask app
 app = Flask(__name__)
-
-#load the config
+# load the config
 app.config.from_object(__name__)
+# init sqlalchemy
+db = SQLAlchemy(app)
 
-# connect to db
-def connect_db():
-    """Connects to the database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-#create db
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-#open db
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-#close db
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+from project import models
 
 
 @app.route('/')
 def index():
-    db = get_db()
-    cur = db.execute('select * from entries order by id desc')
-    entries = cur.fetchall()
+    """Searches the database for entries, then displays them."""
+    entries = db.session.query(models.Post)
     return render_template('index.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Adds new post to the database."""
+    if not session.get('logged_in'):
+        abort(401)
+    new_entry = models.Post(request.form['title'], request.form['text'])
+    db.session.add(new_entry)
+    db.session.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """User login/authentication/session management."""
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
@@ -63,38 +61,28 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
+
 @app.route('/logout')
 def logout():
+    """User logout/authentication/session management."""
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('index'))
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """Add new post to database."""
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute(
-        'insert into entries (title, text) values (?, ?)',
-        [request.form['title'], request.form['text']]
-    )
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
 
-@app.route('/delete/<post_id>', methods=['GET'])
+@app.route('/delete/<int:post_id>', methods=['GET'])
 def delete_entry(post_id):
+    """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db = get_db()
-        db.execute('delete from entries where id = ' + post_id)
-        db.commit()
-        result = {'status': 1, 'message': 'Post Deleted'}
+        db.session.query(models.Post).filter_by(id=post_id).delete()
+        db.session.commit()
+        result = {'status': 1, 'message': "Post Deleted"}
+        flash('The entry was deleted.')
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
     return jsonify(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
-    
